98f2659a2d6479ec47505d55da7fe7ef038e1fc7
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-07-03 12:05:11 +0530

    Vercel Analystic package

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {Provider}  from "react-redux";
 import rootReducer from "./reducer"
 import { configureStore } from "@reduxjs/toolkit";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from '@vercel/analytics/react';
 
 const store = configureStore({
   reducer: rootReducer,
@@ -19,6 +20,7 @@ root.render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <Analytics/>
         <Toaster/>
       </BrowserRouter>
     </Provider>
